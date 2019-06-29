bbae0e71bbbec694e74edf50ee217703531c2295
Juan Miramontes <juan.miramontes@accenture.com>
2019-06-29 13:46:26 -0500

    Removing nav menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import {graphql, useStaticQuery } from "gatsby"
 import "./layout.css"
 import Header from "./header";
 
@@ -21,11 +21,6 @@ export default ({ children }) => {
         title={data.site.siteMetadata.title}
         titleHelper={data.site.siteMetadata.titleHelper} >
       </Header>
-      <ul className="nav">
-        <li> <Link to="/">Home</Link> </li>
-        <li> <Link to="/contact">Contact</Link> </li>
-        <li> <Link to="/about">About</Link> </li>
-      </ul>
       {children}
     </div>
   )
